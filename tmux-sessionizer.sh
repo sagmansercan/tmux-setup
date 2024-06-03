edd054fed22583ac600f495ddd1a4c585a2cb57f
sagmansercan <sagmansercan@gmail.com>
2024-06-03 18:37:20 +0300

    exclude `Library` folder from sessionizer

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -1,7 +1,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~ -mindepth 1 -maxdepth 3 -type d | fzf)
+    selected=$(find ~ -mindepth 1 -maxdepth 3 -type d  \( ! -path "*Library*" \) | fzf)
 fi
 
 if [[ -z $selected ]]; then
