59e1c28aa4ba0dd98e3cfb7d16bda9808bd0d221
sagmansercan <sagmansercan@gmail.com>
2024-09-09 11:08:55 +0300

    enhance path exclusions on folder selection
    to better experience. Eliminates most of the hidden folder
    contents and leaves those folders themselves along.
    
    Also builtin folders like Music, Movies etc. are useless and excluded
    (just content)

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -1,7 +1,25 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~ -mindepth 1 -maxdepth 3 -type d  \( ! -path "*Library*" \) | fzf)
+    selected=$(find ~ \
+        -mindepth 1 -maxdepth 3 -type d \
+        -not -path "*/Library/*" \
+        -not -path "*/.local/*" \
+        -not -path "*/.cache/*" \
+        -not -path "*/.config/*" \
+        -not -path "*/.gradle/*" \
+        -not -path "*/.bun/*" \
+        -not -path "*/.minikube/*" \
+        -not -path "*/.kube/*" \
+        -not -path "*/.vscode/*" \
+        -not -path "*/.rustup/*" \
+        -not -path "*/.cargo/*" \
+        -not -path "*/.oh-my-zsh/*" \
+        -not -path "*/.snowsql/*" \
+        -not -path "*/Music/*" \
+        -not -path "*/Movies/*" \
+        -not -path "*/Pictures/*" \
+        | fzf)
 fi
 
 if [[ -z $selected ]]; then
